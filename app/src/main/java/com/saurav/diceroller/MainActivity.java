ae44b1ebdac6437b32497520462bf2665661e0c8
sauravarya <sauravarya22896@gmail.com>
2021-08-13 08:45:09 +0530

    added feature to show two consecutive rolls of the selected dice through separate buttons

--- app/src/main/java/com/saurav/diceroller/MainActivity.java
+++ app/src/main/java/com/saurav/diceroller/MainActivity.java
@@ -1,12 +1,13 @@
 package com.saurav.diceroller;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -14,21 +15,25 @@ public class MainActivity extends AppCompatActivity
         implements View.OnClickListener, RadioGroup.OnCheckedChangeListener{
 
     private byte result;
+    private byte result2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         RadioGroup diceSizeRG = findViewById(R.id.diceSizeRG);
-        diceSizeRG.check(R.id.d4RadioBtn);
+//        diceSizeRG.check(R.id.d4RadioBtn);
 //        RadioButton d4RadioBtn = findViewById(R.id.d4RadioBtn);
 //        RadioButton d6RadioBtn = findViewById(R.id.d6RadioBtn);
 //        RadioButton d8RadioBtn = findViewById(R.id.d8RadioBtn);
 //        RadioButton d10RadioBtn = findViewById(R.id.d10RadioBtn);
 //        RadioButton d12RadioBtn = findViewById(R.id.d12RadioBtn);
 //        RadioButton d20RadioBtn = findViewById(R.id.d20RadioBtn);
-        Button rollBtn = findViewById(R.id.rollBtn);
-        rollBtn.setOnClickListener(this);
+        Button rollBtn1 = findViewById(R.id.rollBtn1);
+        rollBtn1.setOnClickListener(this);
+
+        Button rollBtn2 = findViewById(R.id.rollBtn2);
+        rollBtn2.setOnClickListener(this);
         try{
 //            setting onCheckedChange listener to the radio group
             diceSizeRG.setOnCheckedChangeListener(this);
@@ -45,31 +50,37 @@ public class MainActivity extends AppCompatActivity
                 Die die4 = new Die((byte) 4);
 //                result = die4.getCurrentSideUp();
                 result = die4.roll();
+                result2 = die4.roll();
                 break;
             case R.id.d6RadioBtn:
                 Die die6 = new Die((byte) 6);
 //                result = die6.getCurrentSideUp();
                 result = die6.roll();
+                result2 = die6.roll();
                 break;
             case R.id.d8RadioBtn:
                 Die die8 = new Die((byte) 8);
 //                result = die8.getCurrentSideUp();
                 result = die8.roll();
+                result2 = die8.roll();
                 break;
             case R.id.d10RadioBtn:
                 Die die10 = new Die((byte) 10);
 //                result = die10.getCurrentSideUp();
                 result = die10.roll();
+                result2 = die10.roll();
                 break;
             case R.id.d12RadioBtn:
                 Die die12 = new Die((byte) 12);
 //                result = die12.getCurrentSideUp();
                 result = die12.roll();
+                result2 = die12.roll();
                 break;
             case R.id.d20RadioBtn:
                 Die die20 = new Die((byte) 20);
 //                result = die20.getCurrentSideUp();
                 result = die20.roll();
+                result2 = die20.roll();
                 break;
             default:
                 Toast.makeText(this, "Select the size to roll the dice by", Toast.LENGTH_SHORT).show();
@@ -79,8 +90,31 @@ public class MainActivity extends AppCompatActivity
 
     public void onClick(View view){
         switch(view.getId()){
-            case R.id.rollBtn:
-                Toast.makeText(this, "die roll result is " + result, Toast.LENGTH_SHORT).show();
+            case R.id.rollBtn1:
+                AlertDialog alertDialog1 = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog1.setTitle("Roll Result Once");
+                alertDialog1.setMessage("The result of the dice roll is: \n" + result);
+                alertDialog1.setButton(AlertDialog.BUTTON_NEUTRAL, "Go Back to Home", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                alertDialog1.show();
+//                Toast.makeText(this, "die roll result is " + result, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rollBtn2:
+                AlertDialog alertDialog2 = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog2.setTitle("Roll Result Twice");
+                alertDialog2.setMessage("The results of two consecutive dice rolls are: \n" + result + " & " + result2);
+                alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "Go Back to Home", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                alertDialog2.show();
+//                Toast.makeText(this, "die roll result is " + result, Toast.LENGTH_SHORT).show();
                 break;
             default:
                 Toast.makeText(this, "View not Implemented", Toast.LENGTH_SHORT).show();
