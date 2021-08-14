718c86c889ba07c2729a3410dc91fd63bfe8ba8e
sauravarya <sauravarya22896@gmail.com>
2021-08-14 09:15:25 +0530

    added functionality to delete history page and option to jump back to home screen

--- app/src/main/java/com/saurav/diceroller/MainActivity.java
+++ app/src/main/java/com/saurav/diceroller/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
--- app/src/main/java/com/saurav/diceroller/SavedDiceActivity.java
+++ app/src/main/java/com/saurav/diceroller/SavedDiceActivity.java
@@ -4,25 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class SavedDiceActivity extends AppCompatActivity {
+public class SavedDiceActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    ArrayList<String> diceRolledList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_dice);
 
+        Button deleteHistoryBtn = findViewById(R.id.deleteHistoryBtn);
+        deleteHistoryBtn.setOnClickListener(this);
+
+        Button backToHomeScreenBtn = findViewById(R.id.backToHomeScreenBtn);
+        backToHomeScreenBtn.setOnClickListener(this);
+
         Intent intent = getIntent();
-        ArrayList<String> diceRolledOnceList = intent.getStringArrayListExtra("diceRolledOnce_key");
+        diceRolledList = intent.getStringArrayListExtra("diceRolledOnce_key");
 
-        ArrayAdapter adapterDiceRolledOnceList = new ArrayAdapter(this, android.R.layout.simple_list_item_1, diceRolledOnceList);
+        ArrayAdapter adapterDiceRolledList = new ArrayAdapter(this, android.R.layout.simple_list_item_1, diceRolledList);
 
-        ListView diceRolledOnceListView = findViewById(R.id.diceRolledHistoryListView);
-        diceRolledOnceListView.setAdapter(adapterDiceRolledOnceList);
+        ListView diceRolledHistoryListView = findViewById(R.id.diceRolledHistoryListView);
+        diceRolledHistoryListView.setAdapter(adapterDiceRolledList);
 
+    }
+
+    public void onClick(View view){
+        Intent m = new Intent(SavedDiceActivity.this, MainActivity.class);
+        switch(view.getId()){
+            case R.id.deleteHistoryBtn:
+                diceRolledList.clear();
+                Toast.makeText(this, "Dice History Cleared", Toast.LENGTH_SHORT).show();
+                this.recreate();
+                break;
+            case R.id.backToHomeScreenBtn:
+                startActivity(m);
+        }
     }
 }
